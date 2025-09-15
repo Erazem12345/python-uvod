c5068d93727c813b9ee077cd28c0721f9c3bcf34
Erazem12345 <erazem3bogataj@gmail.com>
2025-09-15 06:45:14 +0000

    koment

---
+++ main.py
@@ -0,0 +1,68 @@
+"""
+# komentar
+# spremeljivke
+# int (Integer) - celo število
+# spremeljivka - ime= vrednost
+# Starost človeka
+Starost Človeka
+#camel case
+Starost_cloveka=15
+#snake case
+Starost_cloveka=15
+
+# string _ niz znakov
+# "" #shift + 2
+
+# string - niz znkov
+#izračun površine sobe
+sirina = 6
+dolzina = 8
+površina = dolzina* sirina # * = množenje
+print(povrsina)
+
+
+# pretvorba tipov spremeljivk
+
+x = "22"
+# type()
+Print(type(x))
+
+#parsing
+xInt = int(x) #int(string)
+print(type8(xInt))
+
+#input("Koliko si star?)
+# površia spbe
+# input programa
+"""
+sirina = input("Širina tvoje sobe?")
+dolzina = input("Dolžina tvoje sobe?")
+
+sInt=int(sirina)
+dInt = int(dolzina)
+print(sInt * dInt)
+
+# vprašajte uporabnika po širini in dolžini sobe
+# izračunajte obseg sobe
+#dolzina - input (dolzina tvoje sobe?)
+#dolzina = input ( dolzina tvoje sobe)
+print((2 * sInt) + (2 * dInt))
+
+# Program vpraša po številu kepi sladoleda
+# in po ceni kepice
+ # Izračunaj ceno na račun kepice
+ 
+kepica = (int)input("koliko kepic")
+cena = int(input)("Cena ene kepice")
+print(kepica * cena)
+ 
+
+ #Program vpraša šo trenutni uri in minuti
+ # Program vrne koliko sekund je minilo od polnoci ( 24 urni delavni čas)
+
+
+ Ure = int( input(koliko je ura?")
+ minute = ( ure * 60 * 60+ (minute * 60)
+ 
+ 
+ Print ( sekunde))
